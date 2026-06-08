ce52d21c7c27f9176bb39decb353000592b9e19f
Patrick Lee <patrick@PatrickdeiMac.local>
2026-06-08 18:10:04 +0800

    修改靜態輸出設定next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,8 +2,10 @@
 const isStaticExport = process.env.STATIC_EXPORT === 'true';
 
 const nextConfig = {
-  // 只有在靜態導出時才開啟 export
-  ...(isStaticExport ? { output: 'export', images: { unoptimized: true } } : {}),
+  output: 'export',   // 啟用靜態導出
+  images: {
+    unoptimized: true // 靜態導出通常需要關閉預設的圖片優化
+  },
 };
 
 export default nextConfig;
